eeabd5a8ad3888a2e8c3b355fdd6ac93f28f02cd
Abdullah Sowailem <abdullah.sowailem@gmail.com>
2024-05-31 16:36:03 +0300

    add App class.

---
+++ src/App.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * ManasahTech Framework (https://manasahtech.com)
+ *
+ * @license https://github.com/RuyahTech/ManasahTech/blob/main/LICENSE (MIT License)
+ */
+
+declare(strict_types=1);
+
+namespace Slim;
+
+class App
+{
+    /**
+     * Current version
+     *
+     * @var string
+     */
+    public const VERSION = '1.0.0';
+
+}
